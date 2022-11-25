cbccd0408221766eee95c15dea6742a461e39b16
Grant Hale <granthale10@gmail.com>
2022-11-25 10:53:06 -0600

    Debugged logic in generate_state and find_best_action

--- agent.py
+++ agent.py
@@ -43,8 +43,6 @@ class Agent:
             # action = argmax( f(Q(s,a), N(s,a)) )
                 # f( Q(s,a), N(s,a) ) = 1 if N(s,a) < N_e
                 # else                = Q(s,a)
-        
-        # TODO ensure tiebreaking is working correctly
         argmax_list = [0,0,0,0]
         action_list = [utils.RIGHT, utils.LEFT, utils.DOWN, utils.UP]
         for action in action_list:
@@ -53,36 +51,20 @@ class Agent:
             else:
                 argmax_list[action] = self.Q[s_prime][action]
         
+        # Reverse list to match action_list indices
+        argmax_list.reverse()
+        
         # Choose the action at s_prime that maximizes it's Q-value
         max = argmax_list[0]
         best_action = action_list[0]
-
         for idx, val in enumerate(argmax_list):
             if val > max:
-                max = argmax_list
+                max = val
                 best_action = action_list[idx]
 
         return best_action
-    
-    # Helper function to determine if food pellet has been found
-    def food_pellet_found(self, environment, a_prime):
-        head_x = environment[0]
-        head_y = environment[1]
 
-        if a_prime == utils.RIGHT:
-            head_x = environment[0] + 1
-        elif a_prime == utils.LEFT:
-            head_x = environment[0] - 1
-        elif a_prime == utils.DOWN:
-            head_y = environment[1] + 1
-        elif a_prime == utils.UP:
-            head_y = environment[1] - 1
-        
-        if (head_x, head_y) == (environment[3], environment[4]):
-            return True
-        return False
-
-    # Helper function to determine if snake dies
+        # Helper function to determine if snake dies
     def snake_dead(self, s_prime, a_prime):
         # state = (food_dir_x, food_dir_y, adjoining_wall_x, adjoining_wall_y, adjoining_body_top, adjoining_body_bottom, adjoining_body_left, adjoining_body_right)
         
@@ -94,25 +76,15 @@ class Agent:
         if (s_prime[4] == 1 and a_prime == utils.UP) or (s_prime[5] == 1 and a_prime == utils.DOWN) or (s_prime[6] == 1 and a_prime == utils.LEFT) or (s_prime[7] == 1 and a_prime == utils.RIGHT):
             return True
 
-        # TODO track how many steps the snake has taken, ensure it's less than (8 * WIDTH * HEIGHT)
-
         return False
 
-    def calculate_reward(self, environment, s_prime, a_prime):
+    def calculate_reward(self, s_prime, points, dead):
         # 2. From the result of the action on the environment, the agent obtains a reward r_t
-        # if food_pellet_found: 
-                # r_t = 1
-            # if snake_dies:
-                # r_t = -1
-                # and self.reset()
-            # else: 
-                # r_t = -0.1
-            # Update points
         r_t = -.1
-        if self.food_pellet_found(environment, a_prime):
-            r_t = 1
-        elif self.snake_dead(s_prime, a_prime):
+        if dead:
             r_t = -1
+        elif points == self.points + 1: # if food pellet found
+            r_t = 1
         return r_t
 
     def discretize_new_environment(self, environment, a_prime):
@@ -128,14 +100,17 @@ class Agent:
         
         return self.generate_state(environment)
 
-    def update_q_n(self, s_prime, a_prime):
+    def update_q_n(self, s_prime, a_prime, dead):
         # 4. With s_t, a_t, r_t, and s_t+1, the agent can update its Q-value estimate for the state action pair Q(s_t, a_t)
-        
         # a. Update N(s_t, a_t)
-        self.N[self.s][self.a] += 1
+        self.N[s_prime][a_prime] += 1
+
         # b. Update Q(s_t, a_t)
-        alpha = self.C / ( self.C + self.N[s_prime][a_prime] ) 
-        self.Q[self.s][self.a] = self.Q[self.s][self.a] + alpha * (self.points - self.Q[self.s][self.a] + self.gamma * self.Q[s_prime][a_prime] )
+        alpha = self.C / ( self.C + self.N[self.s][self.a] )     
+        if dead:
+            self.Q[self.s][self.a] = self.Q[self.s][self.a] + alpha * (self.points - self.Q[self.s][self.a] + self.gamma * 0 )
+        else:
+            self.Q[self.s][self.a] = self.Q[self.s][self.a] + alpha * (self.points - self.Q[self.s][self.a] + self.gamma * self.Q[s_prime][a_prime] )
 
 
     def act(self, environment, points, dead):
@@ -148,39 +123,40 @@ class Agent:
 
         Tip: you need to discretize the environment to the state space defined on the webpage first
         (Note that [adjoining_wall_x=0, adjoining_wall_y=0] is also the case when snake runs out of the playable board)
-        '''
+        '''        
         # state = (food_dir_x, food_dir_y, adjoining_wall_x, adjoining_wall_y, adjoining_body_top, adjoining_body_bottom, adjoining_body_left, adjoining_body_right)
         s_prime = self.generate_state(environment)
 
-        # TODO Double check that the snake is not past the border walls
-        # TODO Update points
+        # TODO Check that the snake is not past the border walls
+        # TODO Use points input to figure out reward -> update points if food found
         
-        # 1. Choose optimal action based on Q-value or lack of exploration
+        if dead and self._train: # if snake dies, don't look for optimal next step
+            self.update_q_n(s_prime, None, dead)
+            self.reset()
+            return self.a
+
+        # 1. Choose optimal action based on Q-value (or lack of exploration)
         a_prime = self.find_best_action(s_prime)
-
         # 2. From the result of the action on the environment, the agent obtains a reward r_t
-        r_t = self.calculate_reward(environment, s_prime, a_prime)
-
+        r_t = self.calculate_reward(s_prime, points, dead)
+        
         # When t = 0, initialize state and action, disregarding N and Q-table updating
         if self.s == None and self.a == None:
             self.s = s_prime
             self.a = a_prime
-            self.points = r_t
             return a_prime
+            # Do we update points in this situation?
 
-        # 3. The agent then “discretizes” this new environment by generating a state based off of the new, post-action environment
-        s_double_prime = self.discretize_new_environment(environment, a_prime)
+        # 3. TODO The agent then “discretizes” this new environment by generating a state based off of the new, post-action environment
+        # s_double_prime = self.discretize_new_environment(environment, a_prime)
+        # r_t2 = self.calculate_reward(s_double_prime, dead, points)
 
         # 4. With s_t, a_t, r_t, and s_t+1, the agent can update its Q-value estimate for the state action pair Q(s_t, a_t)
-        if self._train: self.update_q_n(s_prime, a_prime)
-
+        if self._train: self.update_q_n(s_prime, a_prime, dead)
         # 5. The agent is now in state s_t+1, and the process repeats
-        if r_t == -1: # if snake dead
-            self.reset()
-        else:
-            self.points = r_t
-            self.s = s_prime
-            self.a = a_prime
+        if r_t == 1: self.points += r_t # if food pellet found, increment global points
+        self.s = s_prime
+        self.a = a_prime
 
         return a_prime
 
@@ -196,16 +172,16 @@ class Agent:
         food_y = environment[4]
         
         food_dir_x = 0
-        if snake_head_x < food_x:
-            food_dir_x = 1 # if head on the left
-        elif snake_head_x > food_x:
-            food_dir_x = 2 # if head on the right
+        if snake_head_x > food_x:
+            food_dir_x = 1 # if food on left of snake head
+        elif snake_head_x < food_x:
+            food_dir_x = 2 # if food on right of snake head
         
         food_dir_y = 0
-        if snake_head_y < food_y:
-            food_dir_y = 1 # if head below food
-        elif snake_head_y > food_y:
-            food_dir_y = 2 # if head above food
+        if snake_head_y > food_y:
+            food_dir_y = 1 # if food above snake head
+        elif snake_head_y < food_y:
+            food_dir_y = 2 # if food below snake head
 
         adjoining_wall_x = 0
         if snake_head_x == 1:
